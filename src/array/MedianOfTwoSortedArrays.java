51be6bd6f818895da82759d37689dc46c501a43b
zhaokuix <kouzhaokui@foxmail.com>
2020-06-22 18:44:14 +0800

    MedianOfTwoSortedArrays

---
+++ src/array/MedianOfTwoSortedArrays.java
@@ -0,0 +1,25 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MedianOfTwoSortedArrays {
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        List<Integer> list = new ArrayList<>();
+        for (int value : nums1) list.add(value);
+        for (int value : nums2) list.add(value);
+        Collections.sort(list);
+        if (list.size() % 2 ==1){
+            return list.get(list.size()/2);
+        }else {
+            return ((long)list.get(list.size()/2) + (long)list.get(list.size()/2-1))/2.0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{1, 3};
+        int[] nums2 = new int[]{2};
+        System.out.println(findMedianSortedArrays(nums1, nums2));
+    }
+}
