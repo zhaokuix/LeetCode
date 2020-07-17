95bccc81f7a3341a93a0bed6fa6bdc6302175067
zhaokuix <kouzhaokui@foxmail.com>
2020-07-17 15:49:51 +0800

    PlusOne -- 类似于大数的加法

---
+++ src/array/PlusOne.java
@@ -0,0 +1,29 @@
+package array;
+
+import java.util.Arrays;
+
+public class PlusOne {
+    public static int[] plusOne(int[] digits) {
+        int[] ans = new int[digits.length + 1];
+        System.arraycopy(digits, 0, ans, 1, ans.length - 1);
+        for (int i = ans.length - 1; i >= 0; i--) {
+            int temp = ans[i] + 1;
+            if (temp < 10){
+                ans[i] = temp;
+                break;
+            }else {
+                ans[i] = 0;
+            }
+        }
+        if (ans[0] == 0){
+            System.arraycopy(ans, 1, digits, 0, ans.length - 1);
+            return digits;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] digit = new int[]{8,9,9};
+        System.out.println(Arrays.toString(plusOne(digit)));
+    }
+}
