5c9c3e833a1ec7f406f86f6cb61409b0b941caa9
zhaokuix <kouzhaokui@foxmail.com>
2020-07-24 16:34:55 +0800

    PascalTriangleII----获得某一行杨辉三角

---
+++ src/array/PascalTriangleII.java
@@ -0,0 +1,31 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PascalTriangleII {
+    public static List<Integer> getRow(int rowIndex) {
+        rowIndex++;
+        if (rowIndex < 1) return new ArrayList<>();
+        if (rowIndex == 1)return  Collections.singletonList(1);
+        List<Integer> last = new ArrayList<>();last.add(1);//保存上一行数据
+        List<Integer> list = null;//当前行数据
+        int s = 2;
+        while (s <= rowIndex){
+            list = new ArrayList<>();
+            list.add(1);
+            for (int i = 1; i < s - 1; i++) {
+                list.add(last.get(i-1) + last.get(i));
+            }
+            list.add(1);
+            last = list;
+            s++;
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getRow(3).toString());
+    }
+}
