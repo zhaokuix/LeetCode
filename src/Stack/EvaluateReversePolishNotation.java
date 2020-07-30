9c783f05d1f0bb877fed2773ee0f684a47a605e2
zhaokuix <kouzhaokui@foxmail.com>
2020-07-30 15:12:38 +0800

    EvaluateReversePolishNotation----逆波兰表达式求值

---
+++ src/Stack/EvaluateReversePolishNotation.java
@@ -0,0 +1,41 @@
+package Stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class EvaluateReversePolishNotation {
+    /**
+     * 逆波兰表达式求值,使用栈
+     * @param tokens 数组
+     * @return 值
+     */
+    public static int evalRPN(String[] tokens) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (String t : tokens) {
+            switch (t){
+                case "+":
+                    stack.push(stack.pop() + stack.pop());
+                    break;
+                case "-":
+                    int num = stack.pop();
+                    stack.push(stack.pop() - num);
+                    break;
+                case "*":
+                    stack.push(stack.pop() * stack.pop());
+                    break;
+                case "/":
+                    int num2 = stack.pop();
+                    stack.push(stack.pop() / num2);
+                    break;
+                default:
+                    stack.push(Integer.valueOf(t));
+            }
+        }
+        return stack.pop();
+    }
+
+    public static void main(String[] args) {
+        String[] tokens = {"2", "1", "+", "3", "*"};
+        System.out.println(evalRPN(tokens));
+    }
+}
