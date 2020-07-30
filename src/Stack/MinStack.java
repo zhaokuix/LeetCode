0e8b193bb8cb3c5874f16fc69c3075bbbf2469bb
zhaokuix <kouzhaokui@foxmail.com>
2020-07-30 16:04:55 +0800

    MinStack----最小栈

--- src/Stack/MinStack.java
+++ src/Stack/MinStack.java
@@ -1,7 +1,40 @@
 package Stack;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.PriorityQueue;
+
 /**
  * https://leetcode-cn.com/problems/min-stack/
  */
 public class MinStack {
+    /** initialize your data structure here. */
+    Deque<Integer> stack;
+    PriorityQueue<Integer> queue;
+    public MinStack() {
+        stack = new ArrayDeque<>();
+        queue = new PriorityQueue<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        queue.add(x);
+    }
+
+    public void pop() {
+        Integer top = stack.pop();
+        queue.remove(top);
+    }
+
+    public int top() {
+        return stack.getFirst();
+    }
+
+    public int getMin() {
+        return queue.peek() == null ? -1:queue.peek();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(-4>>>1);
+    }
 }
